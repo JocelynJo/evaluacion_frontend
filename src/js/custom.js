f899e62c902be49874446a538835fd602dc0b666
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2017-01-07 01:57:47 -0800

    recoge css y responsivo falta calgular costos

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,68 +1,6 @@
 $(document).ready(function(){
 
-	var ciudades = [
-	{
-		name: "arica",
-		distance: 2059
-	},
-	{
-		name: "iquique",
-		distance: 1789
-	},
-	{
-		name: "antofagasta",
-		distance: 1368
-	},
-	{
-		name: "copiapo",
-		distance: 1567
-	},
-	{
-		name: "la_serena",
-		distance: 470
-	},
-	{
-		name: "valparaiso",
-		distance: 116
-	},
-	{
-		name: "rancagua",
-		distance: -84
-	},
-	{
-		name: "talca",
-		distance: -257
-	},
-	{
-		name: "concepcion",
-		distance: -500
-	},
-	{
-		name: "temuco",
-		distance: -690
-	},
-	{
-		name: "valdivia",
-		distance: -848
-	},
-	{
-		name: "puerto_montt",
-		distance: -1032
-	},
-	{
-		name: "coyhaique",
-		distance: -1888
-	},
-	{
-		name: "punta_arenas",
-		distance: -3004
-	},
-	{
-		name: "santiago",
-		distance: 0
-	}
-
-	];
+	var ciudades = get_regiones();
 
 	for(var i=0 ; i<ciudades.length ; ++i){
 		$('.origen').append('<option value="'+(i+1)+'">'+ciudades[i].name+'</option>');
@@ -94,7 +32,7 @@ $(document).ready(function(){
     		ciudad_2 = ciudades[i];
     	}
     }
-    var distancia=Math.abs(ciudad_1.distance-ciudad_2.distance);
+   
 
     $(".buscar").click(function(evento){
 		$(".oculto").css("display", "block");
